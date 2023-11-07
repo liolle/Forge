b8908324b2d72468e4e91486eb70367dba46ab0f
liolle <laurentlionel20@hotmail.com>
2023-11-07 11:12:24 +0100

    [Update] style - add dark theme support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen  flex-col items-center justify-between gap-2 bg-white-main p-2">
-            <section>test</section>
+        <main className="bg-bgc text-content  flex min-h-screen flex-col items-center justify-between gap-2">
+            <section>Forge</section>
         </main>
     );
 }
