4b802e0fb9f17376cbb4390783f6dd57e8809d86
liolle <laurentlionel20@hotmail.com>
2023-11-08 20:38:24 +0100

    [FIX] firebase config

--- src/services/firebase/server.ts
+++ src/services/firebase/server.ts
@@ -1,13 +1,13 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
+const { privateKey } = JSON.parse(process.env.FIREBASE_PRIVATE_KEY || "");
+
 const firebaseAdminConfig = {
     credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY
-            ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
-            : undefined
+        privateKey: privateKey
     })
 };
 
