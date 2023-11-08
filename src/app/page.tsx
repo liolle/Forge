2b82c0d20b56053c1056d6d73a7f6e36321fa1be
liolle <laurentlionel20@hotmail.com>
2023-11-08 16:14:44 +0100

    Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import SignInButton from "../components/client/buttons/SignIn";
 
-export default function Home() {
+export default async function Home() {
     return (
-        <main className="bg-bgc text-content  flex min-h-screen flex-col items-center justify-between gap-2">
-            <section>Forge</section>
+        <main className="flex min-h-screen  flex-col items-center justify-between gap-2 bg-bgc text-content">
+            <section className=" flex h-full w-full flex-1 items-center justify-center">
+                <SignInButton />
+                <span>Forge</span>
+            </section>
         </main>
     );
 }
