ab4a201792910455c7c7dd1037549cefd9e04ee5
liolle <laurentlionel20@hotmail.com>
2023-11-08 18:47:15 +0100

    [Add] firebase validation - server admin validation

---
+++ src/services/firebase/server.ts
@@ -0,0 +1,14 @@
+import { cert, getApps, initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+
+const firebaseAdminConfig = {
+    credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
+    })
+};
+
+export const firebaseAppAdmin =
+    getApps().length === 0 ? initializeApp(firebaseAdminConfig) : getApps()[0];
+export const authAdmin = getAuth(firebaseAppAdmin);
