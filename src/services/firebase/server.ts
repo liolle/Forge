2c3098fec54cc80c6842490050b98b7c7f619667
liolle <laurentlionel20@hotmail.com>
2023-11-08 20:23:20 +0100

    [FIX] attempt firebase private key config

--- src/services/firebase/server.ts
+++ src/services/firebase/server.ts
@@ -6,6 +6,8 @@ const firebaseAdminConfig = {
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_PRIVATE_KEY
+            ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
+            : undefined
     })
 };
 
