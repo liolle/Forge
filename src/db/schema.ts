090612a1ed28f8dd4ff2d062b982dad8c6a2b581
liolle <laurentlionel20@hotmail.com>
2023-11-08 16:13:26 +0100

    [Delete] clean up

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,5 @@
-import {
-    timestamp,
-    pgTable,
-    text,
-    primaryKey,
-    integer
-} from "drizzle-orm/pg-core";
+import { timestamp, pgTable, text } from "drizzle-orm/pg-core";
 import { sql } from "drizzle-orm";
-import { AdapterAccount } from "next-auth/adapters";
 
 export const users = pgTable("user", {
     id: text("id").notNull().primaryKey(),
@@ -18,45 +11,3 @@ export const users = pgTable("user", {
     ),
     created_at: timestamp("created_at").default(sql`now()`)
 });
-
-export const accounts = pgTable(
-    "account",
-    {
-        userId: text("userId")
-            .notNull()
-            .references(() => users.id, { onDelete: "cascade" }),
-        type: text("type").$type<AdapterAccount["type"]>().notNull(),
-        provider: text("provider").notNull(),
-        providerAccountId: text("providerAccountId").notNull(),
-        refresh_token: text("refresh_token"),
-        access_token: text("access_token"),
-        expires_at: integer("expires_at"),
-        token_type: text("token_type"),
-        scope: text("scope"),
-        id_token: text("id_token"),
-        session_state: text("session_state")
-    },
-    (account) => ({
-        compoundKey: primaryKey(account.provider, account.providerAccountId)
-    })
-);
-
-export const sessions = pgTable("session", {
-    sessionToken: text("sessionToken").notNull().primaryKey(),
-    userId: text("userId")
-        .notNull()
-        .references(() => users.id, { onDelete: "cascade" }),
-    expires: timestamp("expires", { mode: "date" }).notNull()
-});
-
-export const verificationTokens = pgTable(
-    "verificationToken",
-    {
-        identifier: text("identifier").notNull(),
-        token: text("token").notNull(),
-        expires: timestamp("expires", { mode: "date" }).notNull()
-    },
-    (vt) => ({
-        compoundKey: primaryKey(vt.identifier, vt.token)
-    })
-);
